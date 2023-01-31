07b388364a997219ec37add5845e8b9a4433b705
paragati <pragatikorke268@gmail.com>
2023-01-31 20:42:58 +0530

    initial ui commit

--- dbconnect.py
+++ dbconnect.py
@@ -9,30 +9,26 @@ mycursor = cnx.cursor()
 #mycursor.execute("DROP TABLE voice")
 #mycursor.execute("CREATE TABLE user (id int primary key AUTO_INCREMENT,fname VARCHAR(255),lname VARCHAR(255),email VARCHAR(255),username VARCHAR(255),password VARCHAR(255),address VARCHAR(255))")
 #mycursor.execute("CREATE TABLE  (id int primary key AUTO_INCREMENT,mname VARCHAR(255),cname VARCHAR(255),context VARCHAR(255),usedfor VARCHAR(255))")
-sql = "INSERT INTO user (id,fname,lname,email,username,password,address) VALUES (%s, %s,%s,%s, %s,%s,%s)"
+#sql = "INSERT INTO user (username,password) VALUES (%s,%s)"
 #sql = "INSERT INTO medicine (id,mname,cname,context,usedfor) VALUES (%s, %s,%s,%s, %s)"
-val = (1,"Ashish","Chavan","ashishchavangkd2003@gmail.com","ashish","ash123","latur")
-mycursor.execute(sql, val)
+#val = ("karpude ","karpude123")
+#mycursor.execute(sql, val)
 
-cnx.commit()
+#cnx.commit()
 
-print(mycursor.rowcount, "record inserted.")
+#print(mycursor.rowcount, "record inserted.")
 
 #mycursor.execute(sql)
 #mycursor.execute("CREATE TABLE voice (id int primary key AUTO_INCREMENT,fname VARCHAR(255),lname VARCHAR(255),email VARCHAR(255),username VARCHAR(255),password VARCHAR(255),address VARCHAR(255))")
 #mycursor.execute("CREATE TABLE voice (id int primary key AUTO_INCREMENT,fname VARCHAR(255),lname VARCHAR(255),email VARCHAR(255),username VARCHAR(255),password VARCHAR(255),address VARCHAR(255))")
 
-#sql = "DELETE FROM medicine WHERE mname= %s"
-#adr = ("Cortana.",)
+sql = "DELETE FROM user WHERE username= %s"
+adr = ("abc",)
+mycursor.execute(sql, adr)
+cnx.commit()
+print(mycursor.rowcount, "record(s) deleted")
 
-#mycursor.execute(sql, adr)
-
-#cnx.commit()
-
-#print(mycursor.rowcount, "record(s) deleted")
-
-
-mycursor.execute("SELECT * FROM user")
+mycursor.execute("SELECT username,password FROM user")
 
 myresult = mycursor.fetchall()
 
